4f9de3754a6462695ccee0beee93e7e7c2ca914d
tianlitl <38939236+tianlitl@users.noreply.github.com>
2020-12-24 15:59:53 +0800

    1st

---
+++ Blizzard_clasify/metrics.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+
+def accuracy(predictions, targets):
+	correct = []
+	for i in range(len(predictions)):
+		if torch.equal(predictions.cpu().long()[i], targets.cpu().long()[i]):
+			correct.append(1)
+		else:
+			correct.append(0)
+	return sum(correct) / len(predictions)
