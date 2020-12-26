e2ab2d86a0ef791bb825401249cdd1ba0310f0be
tianlitl <38939236+tianlitl@users.noreply.github.com>
2020-12-26 18:11:11 +0800

    chang the backbone to EfficientNet-B7

---
+++ train_efficient.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+import torch
+import torch.nn as nn
+from PIL import Image
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from tqdm import tqdm
+
+from Resnet import resnet50
+from dataset import Dataset
+from metrics import accuracy,Cmatrix
+from model import PMG
+from utils import print_msg
+import numpy as np
+from efficientnet_pytorch.model import EfficientNet
+
+
+class Resize(object):
+    def __init__(self, size, interpolation=Image.BILINEAR):
+        self.size = size
+        self.interpolation = interpolation
+
+    def __call__(self, img):
+        ratio = 1
+        w, h = img.size
+        if w / h < ratio:
+            t = int(h * ratio)
+            w_padding = (t - w) // 2
+            img = img.crop((-w_padding, 0, w + w_padding, h))
+        else:
+            t = int(w / ratio)
+            h_padding = (t - h) // 2
+            img = img.crop((0, -h_padding, w, h + h_padding))
+
+        img = img.resize(self.size, self.interpolation)
+
+        return img
+
+
+def _eval(model, dataloader):
+    model.eval()
+    torch.set_grad_enabled(False)
+
+    correct = 0
+    total = 0
+    test_loss = 0
+    correct_com = 0
+
+    all_targ = torch.tensor([]).to(dtype=torch.int64).cuda()
+    all_pred = torch.tensor([]).to(dtype=torch.int64).cuda()
+
+    for test_data in dataloader:
+        X, y = test_data
+        X, y = X.cuda(), y.cuda()
+
+        # y_pred = model(X)
+        output_concat = model(X)
+        outputs_com = output_concat
+        criterion = torch.nn.CrossEntropyLoss().cuda()
+        loss = criterion(output_concat, y)
+
+        test_loss += loss.item()
+        _, predicted = torch.max(output_concat.data, 1)
+        _, predicted_com = torch.max(outputs_com.data, 1)
+        total += y.size(0)
+        # correct += predicted.eq(y.data).cpu().sum()
+        # correct_com += predicted_com.eq(y.data).cpu().sum()
+
+        all_pred = torch.cat((all_pred, predicted_com))
+        all_targ = torch.cat((all_targ, y.to(torch.int64)))
+        # total += y.size(0)
+        correct += accuracy(predicted_com, y) * y.size(0)
+
+    acc = round(correct / total, 4)
+    model.train()
+    torch.set_grad_enabled(True)
+    return acc, all_targ.cpu().numpy(), all_pred.cpu().numpy()
+
+
+if __name__ == '__main__':
+    # creat train dataset
+    data_path = '/data/multi_task/Ubuntu_Code/garbage_classify/DataSet/TrainData'
+    test_data = '/data/multi_task/Ubuntu_Code/garbage_classify/DataSet/TestData'
+    mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+
+    transform = transforms.Compose([Resize((448, 448)),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize(mean=mean, std=std)])
+    train_dataset = Dataset(root=data_path, transform=transform)  # config.py
+    train_loader = DataLoader(train_dataset, batch_size=6, shuffle=True,
+                              pin_memory=True)
+    test_dataset = Dataset(root=test_data, transform=transform)  # config.py
+    test_loader = DataLoader(test_dataset, batch_size=6, shuffle=False,
+                             pin_memory=True)
+
+    # creat model
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2'
+    # net = resnet50(pretrained=True)
+    # model = PMG(net, 448, 43)
+
+    ## Efficientnet
+    # net_name = 'efficientnet-b7'
+
+    model = EfficientNet.from_name('efficientnet-b7')
+    model = EfficientNet.from_pretrained('efficientnet-b7')
+    num_ftrs = model._fc.in_features
+    model._fc = nn.Linear(num_ftrs, 43) #修改为相应的类别数
+    # print(model)
+    model = torch.nn.DataParallel(model)
+    model = model.cuda()
+
+    model = model.cuda()
+    criterion = nn.CrossEntropyLoss().cuda()
+    EPOCH = 30
+    optimizer = torch.optim.SGD(model.parameters(),
+                                lr=0.01,
+                                momentum=0.9,
+                                nesterov=True,
+                                weight_decay=0.)
+    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
+                                                        # 当训练epoch达到milestones值时,初始学习率乘以gamma得到新的学习率;
+                                                        milestones=[10, 20],
+                                                        gamma=0.5)
+    # resume = '/data/multi_task/Ubuntu_Code/garbage_classify/result/best_acc.pth'
+    resume = None
+    if resume:
+        # load checkpoint
+        print('==> Resuming from checkpoint..')
+        assert os.path.isfile(resume), 'Error: no checkpoint directory found!'
+        checkpoint = os.path.dirname(resume)
+        checkpoint = torch.load(resume)
+        best_acc = checkpoint['best_acc']
+        start_epoch = checkpoint['epoch']
+        model.module.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+
+    max_acc = 0
+    for epoch in range(1, EPOCH + 1):
+        # learning rate update
+        epoch_loss = 0
+        correct = 0
+        total = 0
+        progress = tqdm(enumerate(train_loader))  # 进度条
+        for step, train_data in progress:
+            X, y = train_data  # X.dtype is torch.float32, y.dtype is torch.int64
+            X, y = X.cuda(), y.float().cuda()
+
+            # forward
+            y_pred = model(X)
+            # print(f'label:{y} | prediction:{torch.argmax(y_pred, dim=1)}')
+
+            loss = criterion(y_pred, y.long())
+
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if lr_scheduler:
+                lr_scheduler.step()
+                if epoch in lr_scheduler.milestones:
+                    print_msg('Learning rate decayed to {}'.format(lr_scheduler.get_lr()[0]))
+
+            # metrics
+            epoch_loss += loss.item()
+            total += y.size(0)
+            correct += accuracy(torch.argmax(y_pred, dim=1), y) * y.size(0)
+            avg_loss = epoch_loss / (step + 1)
+            train_acc = correct / total
+
+            progress.set_description(
+                'Epoch: {}/{}, loss: {:.6f}, acc: {:.4f}'.format(epoch, EPOCH, avg_loss, train_acc))
+
+
+            if (epoch + 1) % 5 == 0:
+                test_acc, test_target, all_pred = _eval(model, test_loader)
+                # print_msg(f'test_acc:{test_acc}')
+                if test_acc > max_acc:
+                    max_acc = test_acc
+
+                    state = {
+                        'fold': 0,
+                        'epoch': epoch + 1,
+                        'state_dict': model.module.state_dict(),
+                        'train_acc': train_acc,
+                        'acc': test_acc,
+                        'best_acc': max_acc,
+                        'optimizer': optimizer.state_dict(),
+                    }
+                    result_path = '/data/multi_task/Ubuntu_Code/garbage_classify/result'
+                    # torch.save(state, os.path.join(result_path, 'best_acc.pth'))
+                    # print_msg(f'Epoch{epoch} | test_acc:{test_acc}')
+                    torch.save(state, os.path.join(result_path, 'best_acc.pth'))
+                    label = test_target
+                    predic = all_pred
+                    # print(f'eval_label:{label} | prediction:{predic}')
+                    rater_a = np.array(label, dtype=int)
+                    rater_b = np.array(predic, dtype=int)
+                    confusion_matrixs = Cmatrix(rater_a, rater_b, min_rating=0, max_rating=42)
+                    file_out = open('/data/multi_task/Ubuntu_Code/garbage_classify/result/out.txt', mode='w')
+                    file_out.write('Epoch: '+str(epoch)+'\n')
+                    file_out.write(str(confusion_matrixs))
+
+                    print_msg(f'Epoch{epoch} | test_acc:{test_acc} | confusion_matrix:\n{confusion_matrixs}')
+
+
